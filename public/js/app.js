7888c151ca8b632fcc233d5b0cc65e84042bf494
bwv988 <ralph.schlosser@gmail.com>
2021-04-25 21:23:38 +0100

    Trying 2D canvas first.

--- public/js/app.js
+++ public/js/app.js
@@ -7,18 +7,21 @@
 // Define the SVG.
 // FIXME: Change dynamically.
 
-const width = 300
-const height = 300
-const MAXITER = 2
-const LINELEN = 200
-const CENTERX = width / 2
-const CENTERY = height / 2
+
+const MAXITER = 4
+const LINELEN = 20
 
 function setupContext() {
   // The drawing context.
-  var context = d3.path()
+  // var context = d3.path()
   
-  context.moveTo(CENTERX, CENTERY)
+  // context.moveTo(CENTERX, CENTERY)
+
+  // Use a traditional 2D context.
+  var canvas = document.getElementById("canvas")
+  var context = canvas.getContext("2d")
+  
+  context.translate(canvas.width / 2, canvas.height / 4)
 
   return context
 }
@@ -35,7 +38,11 @@ function produceKochKurve(ctx) {
         ctx.rotate((Math.PI / 180) * -60) 
       },
       'F': () => {
+        ctx.beginPath()
+        ctx.moveTo(0, 0)
         ctx.lineTo(0, LINELEN / (kochCurve.iterations + 1))
+        ctx.stroke()
+        ctx.translate(0, LINELEN / (kochCurve.iterations + 1))
       }
      }
   })
@@ -55,18 +62,18 @@ function main() {
 
   // FIXME: Why can't I use the d3.create("svg") pattern from here:
   // https://observablehq.com/@d3/d3-path
-  let svg = d3.select("#svgcontainer")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
+  // let svg = d3.select("#svgcontainer")
+  //     .append("svg")
+  //     .attr("width", width)
+  //     .attr("height", height)
     
   // Call the draw() function.
   // FIXME: Colors, dynamically etc.
   // FIXME: Animate path.
-  svg.append("path")
-      .style("stroke", "black")
-      .style("fill", "none")
-      .attr("d", ctx)
+  // svg.append("path")
+  //     .style("stroke", "black")
+  //     .style("fill", "none")
+  //     .attr("d", ctx)
     
   console.log("Done.");
 }
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Fire up small nodejs server.
 
-var express = require('express');
+var express = require("express");
 
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.listen(8080);
+
+console.log("Listening on port 8080: http://localhost:8080")
